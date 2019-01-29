df0527084faec9b37a5957c8a4b9fcc6f1b2244a
Devin Hayes <devayes@gmail.com>
2019-01-28 22:02:46 -0500

    Update Sinput.php

--- src/Sinput.php
+++ src/Sinput.php
@@ -4,22 +4,12 @@ declare(strict_types=1);
 
 /*
  * Laravel Sinput.
- *
- * (c) Devin Hayes <devayes@gmail.com>
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
  */
 
 namespace devayes\Sinput;
 
 use Illuminate\Http\Request;
 
-/**
- * This is the Sinput class.
- *
- * @author Devin Hayes <devayes@gmail.com>
- */
 class Sinput
 {
     /**
