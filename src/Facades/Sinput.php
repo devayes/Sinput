1b45640af5e138830895361fc0c50e34c3864ec9
Devin Hayes <devayes@gmail.com>
2019-01-28 22:03:48 -0500

    Update Sinput.php

--- src/Facades/Sinput.php
+++ src/Facades/Sinput.php
@@ -2,22 +2,10 @@
 
 declare(strict_types=1);
 
-/*
- * Laravel Sinput.
- *
- * (c) Devin Hayes <devayes@gmail.com>
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
-
 namespace devayes\Sinput\Facades;
 
 use Illuminate\Support\Facades\Facade;
 
-/**
- * @author Devin Hayes <devayes@gmail.com>
- */
 class Sinput extends Facade
 {
     /**
