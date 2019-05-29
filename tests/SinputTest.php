40bfa199ba08b10337133a858a08fd4bde14ca0d
Devin Hayes <devayes@gmail.com>
2019-05-29 16:45:21 -0400

    Tests and fixes

--- tests/SinputTest.php
+++ tests/SinputTest.php
@@ -11,45 +11,121 @@ use Devayes\Tests\Sinput\AbstractTestCase;
 class SinputTest extends AbstractTestCase
 {
 
+    /**
+     * Temporary HTML config to test filtering
+     * @date   2019-05-29
+     * @return array
+     */
+    protected function getHTMLConfig()
+    {
+        return ['HTML.Allowed' => 'b,strong'];
+    }
+
+    /**
+     * Temporary rule set that excludes all HTML
+     * @date   2019-05-29
+     * @return array
+     */
+    protected function getStripConfig()
+    {
+        return [
+            'HTML.Doctype' => 'HTML 4.01 Transitional',
+            'Core.Encoding' => 'UTF-8',
+            'URI.DisableExternalResources' => 1,
+            'HTML.Allowed' => '',
+            'AutoFormat.RemoveEmpty' => 0
+        ];
+    }
+
+    /**
+     * Test object can be created.
+     * @date   2019-05-29
+     * @return [type]     [description]
+     */
     public function testConstruct()
     {
         $sinput = $this->app->make('sinput');
         $this->assertInstanceOf(Sinput::class, $sinput);
     }
 
-    public function testClean()
+    /**
+     * Test our configuration
+     * @date   2019-05-29
+     * @return boolean
+     */
+    public function testConfigIsSet()
     {
         $sinput = $this->app->make('sinput');
+        $default = [
+            'default_ruleset' => 'default',
+            'decode_input' => true,
+            'decode_output' => true
+        ];
+        $this->assertSame($sinput->GetConfig(), $default);
+    }
 
+    /**
+     * Test that our configuration can be set
+     * @date   2019-05-29
+     * @return boolean
+     */
+    public function testSetConfig()
+    {
+        $sinput = $this->app->make('sinput');
+        $sinput->setConfig('default_ruleset', 'html');
+        $this->assertSame('html', $sinput->getConfig('default_ruleset'));
+        $sinput->setConfig('default_ruleset', 'default');
+        $this->assertSame('default', $sinput->getConfig('default_ruleset'));
+    }
+
+    /**
+     * Test clean method, main method all other methods pass through.
+     * @date   2019-05-29
+     * @return [type]     [description]
+     */
+    public function testClean()
+    {
+        $sinput = $this->app->make('sinput');        
         $input = '<b>bold</b> <i>italic</i>';
-        $html = $sinput->clean($input);
-
+        $html = $sinput->clean(
+            $input, 
+            null, 
+            $this->getStripConfig()
+        );
         $this->assertSame('bold italic', $html);
     }
 
+    /**
+     * Test clean with custom inline config.
+     * @date   2019-05-29
+     * @return boolean
+     */
     public function testCleanWithCustomConfig()
     {
         $sinput = $this->app->make('sinput');
 
-        $input = '<b>bold</b> <i>italic</i>';
-        $html = $sinput->clean($input, [
-            'HTML.Allowed' => 'b',
-            'AutoFormat.AutoParagraph' => true
+        $input = '<b>bold</b> <i>italic</i> <del>delete</del>';
+        $html = $sinput->clean($input, null, [
+            'HTML.Allowed' => 'b, del'            
         ]);
 
-        $this->assertSame('<p><b>bold</b> italic</p>', $html);
+        $this->assertSame('<b>bold</b> italic <del>delete</del>', $html);
     }
 
-    public function testAllMethod()
+    /**
+     * Test cleaning and filtering an array
+     * @date   2019-05-29
+     * @return boolean
+     */
+    public function testCleanAndDirtyMethod()
     {
-        $request = new Request();
         $sinput = $this->app->make('sinput');
+        $input = ['foo' => '<b>bold</b> <i>italic</i>'];
 
-        $request->set('foo', '<b>bar</b>');
-        $clean = $sinput->all();
-        $html = $sinput->all('html');
+        $clean = $sinput->clean($input, null, $this->getStripConfig());
+        $html = $sinput->clean($input, null, $this->getHTMLConfig());
 
-        $this->assertSame(['foo' => 'bar'], $clean);
-        $this->assertSame(['foo' => '<b>bar</b>'], $html);
+        $this->assertSame(['foo' => 'bold italic'], $clean);
+        $this->assertSame(['foo' => '<b>bold</b> italic'], $html);
     }
 }
